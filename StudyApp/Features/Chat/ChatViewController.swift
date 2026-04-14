2f2ca4984545df947dbdb88efda73183ed007692
Elias Ferreira <eliasferreira.pro@gmail.com>
2026-04-13 21:23:43 -0300

    use lazy vars for views, use ViewCode Protocol

--- StudyApp/Features/Chat/ChatViewController.swift
+++ StudyApp/Features/Chat/ChatViewController.swift
@@ -9,55 +9,64 @@ import UIKit
 
 class ChatViewController: UIViewController {
     
-    private let tableView = UITableView()
-    private let textField = UITextField()
-    private let button = UIButton(type: .system)
+    // MARK: - UI
+    
+    private(set) lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private(set) lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Digite uma mensagem"
+        return textField
+    }()
+    
+    private(set) lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Enviar", for: .normal)
+        button.addTarget(self, action: #selector(send), for: .touchUpInside)
+        return button
+    }()
+    
+    private(set) lazy var stack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 8
+        return stack
+    }()
+    
+    private(set) lazy var container: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 8
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    // MARK: - Properties
     
     private let viewModel = ChatViewModel()
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupUI()
+        setup()
         bind()
     }
     
-    private func setupUI() {
-        view.backgroundColor = .white
-        
-        tableView.dataSource = self
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        
-        textField.borderStyle = .roundedRect
-        textField.placeholder = "Digite uma mensagem"
-        
-        button.setTitle("Enviar", for: .normal)
-        button.addTarget(self, action: #selector(send), for: .touchUpInside)
-        
-        let stack = UIStackView(arrangedSubviews: [textField, button])
-        stack.axis = .horizontal
-        stack.spacing = 8
-        
-        let container = UIStackView(arrangedSubviews: [tableView, stack])
-        container.axis = .vertical
-        container.spacing = 8
-        container.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(container)
-        
-        NSLayoutConstraint.activate([
-            container.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            container.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            container.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            container.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
-        ])
-    }
+    // MARK: - Functions
     
     private func bind() {
         viewModel.onUpdate = { [weak self] in
             self?.tableView.reloadData()
         }
     }
+    
+    // MARK: - Actions
     
     @objc private func send() {
         guard let text = textField.text, !text.isEmpty else { return }
@@ -66,19 +75,45 @@ class ChatViewController: UIViewController {
     }
 }
 
+// MARK: - UITableViewDataSource
 extension ChatViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewModel.messages.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         let message = viewModel.messages[indexPath.row]
         
         cell.textLabel?.text = message.text
         
         return cell
+    }
+}
+
+// MARK: - ViewCode
+extension ChatViewController: ViewCode {
+    func setupViews() {
+        view.backgroundColor = .white
+        tableView.dataSource = self
+    }
+    
+    func setupHierachy() {
+        stack.addArrangedSubview(textField)
+        stack.addArrangedSubview(button)
+        
+        container.addArrangedSubview(tableView)
+        container.addArrangedSubview(stack)
+        
+        view.addSubview(container)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            container.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            container.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            container.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            container.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        ])
     }
 }
